15ee1eb9f478e690616496b97e98ca26cd288d9f
Isaque <isaque.severino@unifesspa.edu.br>
2025-11-19 23:21:19 -0300

    1.0

--- react-forms/src/App.tsx
+++ react-forms/src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterForm from "./Pages/Register/RegisterForm"
+import LoginForm from "./Pages/Login/LoginForm"
 
 export default function App() {
   return (
-   <div>
-     <RegisterForm />
-   </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
--- react-forms/src/Pages/Register/RegisterForm.tsx
+++ react-forms/src/Pages/Register/RegisterForm.tsx
@@ -1,48 +1,109 @@
 import './RegisterForm.css'
+import { Link, useNavigate } from 'react-router-dom'
+import { useForm } from "react-hook-form";
+import { useEffect } from 'react';
+
+import src from '../../Assets/NavHeader.svg'
+import background from '../../Assets/Login_Background.jpg'
+
+interface RegisterFormData {
+    nome: string;
+    email: string;
+    senha: string;
+}
+
+const savedData = localStorage.getItem("registerFormData");
+const defaultValues = savedData ? JSON.parse(savedData) : {};
 
 export default function RegisterForm() {
+
+    const { register, handleSubmit, watch, formState: { errors }, reset } = useForm<RegisterFormData>({ defaultValues });
+
+    const formValues = watch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        localStorage.setItem("registerFormData", JSON.stringify(formValues));
+    }, [formValues]);
+
+    const onSubmit = (data: RegisterFormData) => {
+        console.log(data);
+        localStorage.removeItem("registerFormData");
+        reset();
+        navigate("/login");
+    };
+
     return (
-        <div className="w-full h-screen flex items-center justify-center font-poppins">
-            <div className='inset-shadow-sms p-8 rounded-lg border border-gray-300 flex flex-col'>
-                <h1 className="text-2xl font-bold">Crie sua Conta</h1>
-                <span className='text-sm text-gray-500 mb-10'>Informe seu nome, e-mail e senha</span>
+        <div className="w-full h-screen flex justify-end font-poppins" style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
 
-                <form className="flex flex-col gap-5 w-80">
-                    <div className='flex flex-col'>
-                        <label className='text-xs font-bold text-gray-400'>NOME</label>
-                        <input
-                            type="text"
-                            placeholder="Digite o nome completo"
-                            className="h-10 text-sm bg-white border-b border-gray-300  outline-blue-700 font-poppins"
-                        />
-                    </div>
+            <div className='w-2/4 h-auto flex flex-col mt-3 bg-white items-center justify-center font-poppins border-t-2 border-l-2 rounded-tl-3xl'>
+                <img className='mb-8' src={src} alt="Navigation Header" />
 
-                    <div className='flex flex-col'>
-                        <label className='text-xs font-bold text-gray-400'>E-MAIL</label>
-                        <input
-                            type="text"
-                            placeholder="exemplo@mail.com"
-                            className="h-10 text-sm bg-white border-b border-gray-300  outline-blue-700 font-poppins"
-                        />
-                    </div>
+                <div className='inset-shadow-sms p-8 rounded-lg border border-gray-300 flex flex-col'>
+                    <h1 className="text-2xl font-bold">Crie sua Conta</h1>
+                    <span className='text-sm text-gray-500 mb-10'>Informe seu nome, e-mail e senha</span>
 
-                    <div className='flex flex-col'>
-                        <label className='text-xs font-bold text-gray-400'>SENHA</label>
-                        <input
-                            type="text"
-                            placeholder="Digite sua senha"
-                            className="h-10 text-sm bg-white border-b border-gray-300  outline-blue-700 font-poppins"
-                        />
-                    </div>
+                    <form className="flex flex-col gap-5 w-80" onSubmit={handleSubmit(onSubmit)}>
+                        <div className='flex flex-col'>
+                            <label className='text-xs font-bold text-gray-500'>NOME</label>
+                            <input
+                                type="text"
+                                placeholder="Digite o nome completo"
+                                className="h-10 text-sm bg-white border-b border-gray-300  outline-blue-700 font-poppins"
+                                {...register("nome", { required: "Nome obrigatório" })}
+                            />
+                            {errors.nome && <p>{errors.nome.message}</p>}
+                        </div>
 
-                    <button
-                        type="submit"
-                        className="h-13 bg-blue-800 rounded-md text-white font-semibold text-base px-3 cursor-pointer transition-filter duration-200 hover:brightness-90 border-none"
-                    >
-                        Salvar
-                    </button>
-                </form>
+                        <div className='flex flex-col'>
+                            <label className='text-xs font-bold text-gray-500'>E-MAIL</label>
+                            <input
+                                type="text"
+                                placeholder="exemplo@mail.com"
+                                className="h-10 text-sm bg-white border-b border-gray-300  outline-blue-700 font-poppins"
+                                {...register("email", { required: "E-mail obrigatório" })}
+                            />
+                            {errors.email && <p>{errors.email.message}</p>}
+                        </div>
+
+                        <div className='flex flex-col mb-3'>
+                            <label className='text-xs font-bold text-gray-500'>SENHA</label>
+                            <input
+                                type="password"
+                                placeholder="Digite sua senha"
+                                className="h-10 text-sm bg-white border-b border-gray-300  outline-blue-700 font-poppins"
+                                {...register("senha", { required: "Senha obrigatória", minLength: { value: 6, message: "Mínimo 6 dígitos" } })}
+                            />
+                            {errors.senha && <p className="text-xs text-gray-400 mt-2">{errors.senha.message}</p>}
+                        </div>
+
+                            <button
+                                type="submit"
+                                className="h-13 w-full bg-black rounded-md text-white font-semibold text-base px-3 cursor-pointer transition-filter duration-200 hover:brightness-90 border-none"
+                            >
+                                Cadastrar
+                            </button>
+                  
+                    </form>
+
+                </div>
+
+                <div className='inset-shadow-sms p-8 mt-5 rounded-lg border border-gray-300 flex flex-col w-97'>
+                    <h1 className="text-1.5xl font-bold">Já uma conta?</h1>
+                    <span className='text-sm text-gray-500 mb-8'>Entre agora mesmo</span>
+
+                    <Link to="/login">
+                        <button
+                            type="submit"
+                            className="h-13 w-full bg-gray-200 rounded-md text-black font-semibold text-base px-3 cursor-pointer transition-filter duration-200 hover:brightness-90 border-none"
+                        >
+                            Acessar conta
+                        </button>
+                    </Link>
+
+                </div>
             </div>
+
         </div>
     )
 }
